845dc31077a1a1b6fb9fcb6ed5e7080cd5eb63c0
irisv19 <143832578+irisv19@users.noreply.github.com>
2024-02-08 08:34:27 -0800

    demo done

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
 // Add Event listners when DOM is ready
 document.addEventListener("DOMContentLoaded", function () {
 
-    // check for buttons exist
-    if ( document.querySelectorAll('.button') !== null) {
-        let buttons = document.querySelectorAll('.button');
-        buttons.forEach(button => button.addEventListener('click', processForm))
-    }
+  // check for buttons exist
+  if (document.querySelectorAll('.button') !== null) {
+    let buttons = document.querySelectorAll('.button');
+    buttons.forEach(button => button.addEventListener('click', processForm))
+  }
 
-    // optionally update when fields update
-    // tbd
+  // optionally update when fields update
+  // tbd
+  document.getElementById('name').addEventListener('change', function (e) {
+    let nameElement = document.getElementById('nameTag');
+    nameElement.innerText = e.target.value;
+  });
 
-    // process form Data
-    document.getElementById("myForm").addEventListener("submit", function (e) {
-        e.preventDefault();
-      
-        var formData = new FormData(form);
-        // output as an object
-        console.log(Object.fromEntries(formData));
-      
-        // ...or iterate through the name-value pairs
-        for (var pair of formData.entries()) {
-          console.log(pair[0] + ": " + pair[1]);
-        }
-      });
-    // Log readiness to console
-    console.log("Ready");
+  document.getElementById('type').addEventListener('change', function (e) {
+    let infoElement = document.getElementById('info');
+    infoElement.innerText = e.target.value;
+  });
+
+
+  // process form Data
+  document.getElementById("myForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    console.log(e.target);
+
+    var formData = new FormData(e.target);
+    formData = Object.fromEntries(formData);
+    // output as an object
+    // console.log(Object.fromEntries(formData));
+
+    // ...or iterate through the name-value pairs
+  //   for (var pair of formData.entries()) {
+  //     console.log(pair[0] + ": " + pair[1]);
+  //   }
+  
+  let nameElement = document.getElementById('nameTag');
+  let textElement = document.getElementById('info');
+
+  nameElement.innerText = formData.name;
+  textElement.innerText = formData.type;
+});
+// Log readiness to console
+  console.log("Ready");
 });
 
-function processForm(form){
+function processForm(form) {
 
 }
